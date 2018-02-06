6185f5f279f04fd0ba7c6b4a0479a0b3870cd25e
Victor Igbokwe <vicsstar@yahoo.com>
2018-02-06 12:38:28 +0200

    Removed unnecessary cases.

--- chat-handler.js
+++ chat-handler.js
@@ -3,13 +3,13 @@ const WebSocket = require('ws')
 let wsServer;
 let users = [];
 const channels = [
-  { name: 'general', description: '', id: '1001' },
-  { name: 'random', description: '', id: '1002' },
-  { name: 'polymer', description: '', id: '1003' },
-  { name: 'emberjs', description: '', id: '1004' },
-  { name: 'ios', description: '', id: '1005' },
-  { name: 'angularjs', description: '', id: '1016' },
-  { name: 'reactjs', description: '', id: '1017' }
+  { name: 'general', description: 'Welcome to Nifty PRO!', id: '1001' },
+  { name: 'random', description: 'Here you can post random messages and things that don\'t necessarily matter.', id: '1002' },
+  { name: 'polymer', description: 'Polymer library discussions only!', id: '1003' },
+  { name: 'emberjs', description: 'Ask and answer questions on the EmberJS framework.', id: '1004' },
+  { name: 'ios', description: 'This is the channel for all iOS devs - iOS talks and iOS questions', id: '1005' },
+  { name: 'angularjs', description: 'Speak Angular? Contribute - help others or ask questions here.', id: '1016' },
+  { name: 'reactjs', description: 'This channel is for you if you work with the ReactJS library.', id: '1017' }
 ];
 
 function chatConnection(wss) {
@@ -27,10 +27,10 @@ function chatConnection(wss) {
             users.push({ nickname: data.nickname });
           }
           sendChannelList(ws);
-          broadcast({
-            type: 'USER_LIST',
-            users
-          }, ws);
+          // broadcast({
+          //   type: 'USER_LIST',
+          //   users
+          // }, ws);
           break;
         }
         case 'USER_LEAVE': {
@@ -43,17 +43,6 @@ function chatConnection(wss) {
         }
         case 'CHANNEL_LIST': {
           sendChannelList(ws);
-          break;
-        }
-        // TODO: consider removing this case. Perhaps not useful as the next case (ROOM_JOIN)
-        // takes care of user listing as per the front-end chat app model.
-        case 'USER_LIST': {
-          const users = users // or findUsersInRoom(data.roomId) for members specific to this room.
-
-          send({
-            type: 'USER_LIST',
-            users
-          }, ws);
           break;
         }
         case 'MESSAGE_ADD':
@@ -98,7 +87,8 @@ function broadcast(data, ws) {
 function sendChannelList(ws) {
   send({
     type: 'CHANNEL_LIST',
-    channels
+    channels,
+    users
   }, ws);
 }
 
